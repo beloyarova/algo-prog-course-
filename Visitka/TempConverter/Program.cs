ea1ea562bbbf482f3c5b736263c4ace09b0716ad
beloyarova <abeloarova@gmail.com>
2026-09-08 16:37:27 +0300

    Lab01 task 2: temperature converter

---
+++ Visitka/TempConverter/Program.cs
@@ -0,0 +1,8 @@
+﻿double celsius = 19.5;
+const int F = 32;
+const double K = 273.15;
+
+double F1 = celsius * (9 / 5) + F;
+double K1 = celsius + K;
+
+Console.WriteLine($"{celsius}°C = {F1}°F = {K1}K");
